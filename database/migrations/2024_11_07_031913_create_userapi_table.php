474a666e2a79016331ac69c25bd6951dcdb9403b
V1cente12 <jvicentecv@gmail.com>
2024-11-07 01:36:56 -0400

    create userapi migrate

--- database/migrations/2024_11_07_031913_create_userapi_table.php
+++ database/migrations/2024_11_07_031913_create_userapi_table.php
@@ -15,7 +15,19 @@ class CreateUserapiTable extends Migration
     {
         Schema::create('userapi', function (Blueprint $table) {
             $table->id();
+            $table->string('uid')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email', 100)->unique();
+            $table->string('password');
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('phone_2')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->char('gender', 1)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
